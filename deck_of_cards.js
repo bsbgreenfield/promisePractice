b1dade8005e53ebaf825aabcc80e767ba5e3acbf
Benjamin Greenfield <benjamingreenfield@Benjamins-MacBook-Pro.local>
2023-03-22 14:57:40 -0700

    upload project

--- deck_of_cards.js
+++ deck_of_cards.js
@@ -1,2 +1,53 @@
+baseURL = 'https://deckofcardsapi.com/api/deck'
 
+
+/* 
+axios.get(`${baseURL}/new/shuffle/?deck_count=1`)
+.then((response) => {
+    const card_promises = []
+    for (let i = 1; i < 3; i++){
+        card_promises.push(axios.get(`${baseURL}/${response.data.deck_id}/draw/?count=1`))
+    }
+    Promise.all(card_promises)
+    .then(card_promises => {
+        card_promises.forEach(card => {
+        for (resultCard of card.data.cards){
+            console.log(resultCard.value, resultCard.suit)
+        }
+    });
+})
+}) */
+
+const cardContainer = document.querySelector('#card-container')
+
+function generateCardMarkup(card){
+    let cardWrapper = document.createElement('div')
+    let cardImage = document.createElement('img')
+    cardImage.src = card.image
+
+    cardWrapper.appendChild(cardImage)
+    cardContainer.appendChild(cardWrapper)
+}
+
+axios.get(`${baseURL}/new/shuffle/?deck_count=1`)
+.then((response) => {
+    let button = document.createElement('button')
+    button.innerText = 'Click For Card'
+    axios.get(`${baseURL}/${response.data.deck_id}/draw/?count=52`)
+    .then((allcards) => {
+        document.body.prepend(button)
+        let counter = 0
+        button.addEventListener('click', function(){
+            if(counter <= 51){
+                generateCardMarkup(allcards.data.cards[counter])
+                counter ++;
+            }
+           else{
+            button.innerText = 'no more cards'
+            button.style.border = '1px solid red'
+           }
+        })
+        console.log(allcards)
+    })
+})
 
--- number_promise.js
+++ number_promise.js
@@ -1,1 +1,58 @@
+let myNumber = 99
+let baseURL = `http://numbersapi.com/`
+
+
+//part 1
+axios.get(`${baseURL}/${myNumber}/trivia?json`)
+.then((data) => {
+    console.log(data.data)
+})
+.catch((err)=> {
+    console.log(err)
+})
+
+//part 2
+axios.get(`${baseURL}70..74/trivia?json`)
+.then((data) => {
+    console.log(data.data)
+    for (let fact of Object.values(data.data)){
+        let numberFact = document.createElement('p')
+        numberFact.innerText = fact
+        document.body.appendChild(numberFact)
+    }
+ 
+})
+.catch((err)=> {
+    console.log(err)
+})
+
+
+//part 3 
+
+function createFactMarkup(fact){
+    let numberFact = document.createElement('p')
+    numberFact.innerText = fact
+    document.body.appendChild(numberFact)
+}
+
+axios.get(`${baseURL}${myNumber}/trivia?json`)
+.then((data) => {
+    createFactMarkup(data.data.text)
+    return axios.get(`${baseURL}${myNumber}/trivia?json`)
+})
+.then((data) => {
+    createFactMarkup(data.data.text)
+    return axios.get(`${baseURL}${myNumber}/trivia?json`)
+})
+.then((data) => {
+    createFactMarkup(data.data.text)
+    return axios.get(`${baseURL}${myNumber}/trivia?json`)
+})
+.then((data) => {
+    createFactMarkup(data.data.text)
+    return axios.get(`${baseURL}${myNumber}/trivia?json`)
+})
+.catch((err) => {
+    console.log(err)
+})
 
